30f55f0ba2c3a063361bf77dea537c6b3a09234d
Gustavo Mendonca <gustavogmend@gmail.com>
2015-12-08 16:30:45 -0600

    starting to understand something

---
+++ fft.c
@@ -0,0 +1,91 @@
+
+/*
+ ------------------------------------------------------------------------
+ FFT1D            c_fft1d(r,i,-1)
+ Inverse FFT1D    c_fft1d(r,i,+1)
+ ------------------------------------------------------------------------
+*/
+/* ---------- FFT 1D
+   This computes an in-place complex-to-complex FFT
+   r is the real and imaginary arrays of n=2^m points.
+   isign = -1 gives forward transform
+   isign =  1 gives inverse transform
+*/
+
+#include <assert.h>
+#include <math.h>
+
+typedef struct {float r; float i;} complex;
+static complex ctmp;
+
+#define C_SWAP(a,b) {ctmp=(a);(a)=(b);(b)=ctmp;}
+
+void c_fft1d(complex *r, int      n, int      isign)
+{
+   int     m,i,i1,j,k,i2,l,l1,l2;
+   float   c1,c2,z;
+   complex t, u;
+
+   if (isign == 0) return;
+
+   /* Do the bit reversal */
+   i2 = n >> 1;
+   j = 0;
+   for (i=0;i<n-1;i++) {
+      if (i < j)
+         C_SWAP(r[i], r[j]);
+      k = i2;
+      while (k <= j) {
+         j -= k;
+         k >>= 1;
+      }
+      j += k;
+   }
+
+   /* m = (int) log2((double)n); */
+   for (i=n,m=0; i>1; m++,i/=2);
+
+   /* Compute the FFT */
+   c1 = -1.0;
+   c2 =  0.0;
+   l2 =  1;
+   for (l=0;l<m;l++) {
+      l1   = l2;
+      l2 <<= 1;
+      u.r = 1.0;
+      u.i = 0.0;
+      for (j=0;j<l1;j++) {
+         for (i=j;i<n;i+=l2) {
+            i1 = i + l1;
+
+            /* t = u * r[i1] */
+            t.r = u.r * r[i1].r - u.i * r[i1].i;
+            t.i = u.r * r[i1].i + u.i * r[i1].r;
+
+            /* r[i1] = r[i] - t */
+            r[i1].r = r[i].r - t.r;
+            r[i1].i = r[i].i - t.i;
+
+            /* r[i] = r[i] + t */
+            r[i].r += t.r;
+            r[i].i += t.i;
+         }
+         z =  u.r * c1 - u.i * c2;
+
+         u.i = u.r * c2 + u.i * c1;
+         u.r = z;
+      }
+      c2 = sqrt((1.0 - c1) / 2.0);
+      if (isign == -1) /* FWD FFT */
+         c2 = -c2;
+      c1 = sqrt((1.0 + c1) / 2.0);
+   }
+
+   /* Scaling for inverse transform */
+   if (isign == 1) {       /* IFFT*/
+      for (i=0;i<n;i++) {
+         r[i].r /= n;
+         r[i].i /= n;
+      }
+   }
+}
---
+++ project.c
@@ -0,0 +1,107 @@
+#include <assert.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct {float r; float i;} complex;
+static complex ctmp;
+
+#define C_SWAP(a,b) {ctmp=(a);(a)=(b);(b)=ctmp;}
+
+#define N 512
+
+void c_fft1d(complex *r, int      n, int      isign)
+{
+   int     m,i,i1,j,k,i2,l,l1,l2;
+   float   c1,c2,z;
+   complex t, u;
+
+   if (isign == 0) return;
+
+   /* Do the bit reversal */
+   i2 = n >> 1;
+   j = 0;
+   for (i=0;i<n-1;i++) {
+      if (i < j)
+         C_SWAP(r[i], r[j]);
+      k = i2;
+      while (k <= j) {
+         j -= k;
+         k >>= 1;
+      }
+      j += k;
+   }
+
+   /* m = (int) log2((double)n); */
+   for (i=n,m=0; i>1; m++,i/=2);
+
+   /* Compute the FFT */
+   c1 = -1.0;
+   c2 =  0.0;
+   l2 =  1;
+   for (l=0;l<m;l++) {
+      l1   = l2;
+      l2 <<= 1;
+      u.r = 1.0;
+      u.i = 0.0;
+      for (j=0;j<l1;j++) {
+         for (i=j;i<n;i+=l2) {
+            i1 = i + l1;
+
+            /* t = u * r[i1] */
+            t.r = u.r * r[i1].r - u.i * r[i1].i;
+            t.i = u.r * r[i1].i + u.i * r[i1].r;
+
+            /* r[i1] = r[i] - t */
+            r[i1].r = r[i].r - t.r;
+            r[i1].i = r[i].i - t.i;
+
+            /* r[i] = r[i] + t */
+            r[i].r += t.r;
+            r[i].i += t.i;
+         }
+         z =  u.r * c1 - u.i * c2;
+
+         u.i = u.r * c2 + u.i * c1;
+         u.r = z;
+      }
+      c2 = sqrt((1.0 - c1) / 2.0);
+      if (isign == -1) /* FWD FFT */
+         c2 = -c2;
+      c1 = sqrt((1.0 + c1) / 2.0);
+   }
+
+   /* Scaling for inverse transform */
+   if (isign == 1) {       /* IFFT*/
+      for (i=0;i<n;i++) {
+         r[i].r /= n;
+         r[i].i /= n;
+      }
+   }
+}
+
+void getData(char fileName[12], float data[N][N]){
+    FILE *fp = fopen(fileName, "r");
+
+    int i, j;
+
+    for (i=0;i<N;i++)
+        for (j=0;j<N;j++)
+            fscanf(fp,"%g",&data[i][j]);
+
+    fclose(fp);
+}
+
+
+int main(){
+    float data1[N][N], data2;
+
+    char fileName1[12] = "sample/1_im1";
+    char fileName2[12] = "sample/1_im2";
+
+    getData(fileName1, data1);
+    getData(fileName2, data2);
+
+
+    return 0;
+}
